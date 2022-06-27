6de0c3451e6918d4d0350cfc87b990de7d459ed1
syhwang4223 <syhwang4223@gmail.com>
2022-06-27 23:26:19 +0900

    [TEST] TEST

--- src/main/java/hello/ebookstore/controller/CommentController.java
+++ src/main/java/hello/ebookstore/controller/CommentController.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
